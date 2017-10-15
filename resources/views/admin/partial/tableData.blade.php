2ec20218355bb6097199ecb3c101501089ec2e7e
Tran Thanh <lethanhtdt@gmail.com>
2017-10-15 13:05:13 +0700

    update tableData

--- resources/views/admin/partial/tableData.blade.php
+++ resources/views/admin/partial/tableData.blade.php
@@ -231,3 +231,59 @@
         </div>
     </div>
 @endsection
+@section('jstable')
+        
+        <!--js table-->
+        <script src="{{asset('backend/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.bootstrap.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.buttons.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/buttons.bootstrap.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/jszip.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/pdfmake.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/vfs_fonts.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/buttons.html5.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/buttons.print.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.fixedHeader.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.keyTable.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.responsive.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/responsive.bootstrap.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.scroller.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.colVis.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables/dataTables.fixedColumns.min.js')}}"></script>
+
+        <!-- init -->
+        <script src="{{asset('backend/pages/jquery.datatables.init.js')}}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#datatable').dataTable();
+            $('#datatable-keytable').DataTable({keys: true});
+            $('#datatable-responsive').DataTable();
+            $('#datatable-colvid').DataTable({
+                "dom": 'C<"clear">lfrtip',
+                "colVis": {
+                    "buttonText": "Change columns"
+                }
+            });
+            $('#datatable-scroller').DataTable({
+                ajax: "../plugins/datatables/json/scroller-demo.json",
+                deferRender: true,
+                scrollY: 380,
+                scrollCollapse: true,
+                scroller: true
+            });
+            var table = $('#datatable-fixed-header').DataTable({fixedHeader: true});
+            var table = $('#datatable-fixed-col').DataTable({
+                scrollY: "300px",
+                scrollX: true,
+                scrollCollapse: true,
+                paging: false,
+                fixedColumns: {
+                    leftColumns: 1,
+                    rightColumns: 1
+                }
+            });
+        });
+        TableManageButtons.init();
+
+    </script>
+@endsection
